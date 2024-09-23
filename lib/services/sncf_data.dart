d6f1430d8a7517cff0ee4c5a5c0652ec4a6481dd
leminecraftien2.0 <leminecraftien2.0@gmail.com>
2024-09-23 10:11:25 +0000

    wip

--- lib/services/sncf_data.dart
+++ lib/services/sncf_data.dart
@@ -15,4 +15,37 @@ class SncfData {
     }
     throw Exception('Failed to load found objects');
   }
+
+  // Rechercher des objets trouvés selon les filtres
+  Future<List<FoundObject>> searchFoundObjects({
+    String? stationName, // Filtrer par nom de la gare
+    String? objectType, // Filtrer par type d'objet
+    DateTime? dateMin, // Filtrer par date minimale
+    DateTime? dateMax, // Filtrer par date maximale
+  }) async {
+    // Construction dynamique de l'URL avec les filtres
+    String query = '$apiUrl&sort=date%20DESC';
+
+    if (stationName != null && stationName.isNotEmpty) {
+      query += '&q=gc_obo_gare_origine_r_name:$stationName';
+    }
+    if (objectType != null && objectType.isNotEmpty) {
+      query += '%20AND%20gc_obo_type_c:$objectType';
+    }
+    if (dateMin != null) {
+      final dateMinStr = dateMin.toIso8601String();
+      query += '%20AND%20date>=$dateMinStr';
+    }
+    if (dateMax != null) {
+      final dateMaxStr = dateMax.toIso8601String();
+      query += '%20AND%20date<=$dateMaxStr';
+    }
+
+    final response = await http.get(Uri.parse(query));
+    if (response.statusCode == 200) {
+      final List<dynamic> data = json.decode(response.body)['records'];
+      return data.map((item) => FoundObject.fromJson(item['fields'])).toList();
+    }
+    throw Exception('Failed to search found objects');
+  }
 }
